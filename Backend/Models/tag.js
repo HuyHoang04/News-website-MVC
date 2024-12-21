a72795beeca5689a68bd3da1fe6c62599ba22dd2
PhatDos <162654433+PhatDos@users.noreply.github.com>
2024-12-21 16:25:41 +0700

    Update tag.js

--- Backend/Models/tag.js
+++ Backend/Models/tag.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-const categorySchema = new mongoose.Schema({
+const tagSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   content: { type: String, required: true, unique: true }
 });
 
-export const Category = mongoose.model("Tag", tagSchema);
+export const Tag = mongoose.model("Tag", tagSchema);
