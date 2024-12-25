4b74b4264c8a727ea420ed4416cc2a7048037492
La Huy Hoang <lahuyhoang04@gmail.com>
2024-12-25 23:28:36 +0700

    host

--- Backend/main.js
+++ Backend/main.js
@@ -654,7 +654,7 @@ app.get("/latest", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
